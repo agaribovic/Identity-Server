5eb0e8a135ab99a27287236e65e17f8ecdb2441a
Zakir Cinjarevic <zakircin@gmail.com>
2019-03-19 08:53:02 +0100

    master push

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    port: 5000,
+    secret: 'P0p0k4t3p3t1',
+    mongo: 'mongodb://localhost:27017/TimeKeeperIdentity',
+    //mongo:'mongodb://mongo:27017/identity',
+    token: '',
+    currentUser: {}
+}
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import User from "../models/user.model";
+import config from "../../config";
+import base64 from "../helpers/base64";
+import jwt from "jsonwebtoken";
+
+const login = (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  User.findOne({ email: username }, (err, data) => {
+    if (err || !data) {
+      res.status(401).send("User does not exists");
+    } else {
+      if (data.authenticate(password)) {
+        config.currentUser = {
+          _id: data._id,
+          sub: data.email,
+          cli: "TimeKeeper",
+          role: "user",
+          name: data.name,
+          exp: new Date().getTime() / 1000 + 3600
+        };
+        let token = jwt.sign(config.currentUser, config.secret);
+        res.status(200).send(token);
+      } else {
+        res.status(401).send("User does not exists");
+      }
+    }
+  });
+};
+
+const signed = (req, res, next) => {
+  let token = req.headers.authorization;
+  console.log("token: " + token);
+  if (token === undefined) {
+    res.status(401).send("Unathorized access");
+  } else {
+    jwt.verify(token.substring(7), config.secret, (err, result) => {
+      if (err) {
+        res.status(401).send("Invalid token");
+      } else {
+        config.currentUser = result;
+        let exp = result.exp - new Date().getTime() / 1000;
+        next();
+      }
+    });
+  }
+};
+
+export default { login, signed };
